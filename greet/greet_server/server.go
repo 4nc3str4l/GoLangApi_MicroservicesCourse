98f1dcd4b1e5ef1bd4ad3634ae4f9a680d9743e1
4nc3str4l <cmuri_10@hotmail.com>
2019-01-11 10:07:22 +0100

    Defined an streaming server

--- greet/greet_server/server.go
+++ greet/greet_server/server.go
@@ -12,11 +12,24 @@ import (
 
 type server struct{}
 
+// Greet implementation
 func (*server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.GreetResponse, error) {
-	fmt.Printf("Greet function was invoked with %v", req)
+	fmt.Printf("Greet function was invoked with %v\n", req)
 	firstName := req.GetGreeting().GetFirstName()
 	result := "Hello " + firstName
 	res := &greetpb.GreetResponse{
+		Result: result,
+	}
+	return res, nil
+}
+
+// Sum implementation
+func (*server) Sum(ctx context.Context, req *greetpb.SumRequest) (*greetpb.SumResponse, error) {
+	fmt.Printf("Greet function was invoked with %v\n", req)
+	p1 := req.GetParameters().GetP1()
+	p2 := req.GetParameters().GetP2()
+	result := p1 + p2
+	res := &greetpb.SumResponse{
 		Result: result,
 	}
 	return res, nil
@@ -31,6 +44,9 @@ func main() {
 
 	s := grpc.NewServer()
 	greetpb.RegisterGreetServiceServer(s, &server{})
+
+	// Register the calculator service
+	greetpb.RegisterCalculatorServiceServer(s, &server{})
 
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to server: %v", err)
